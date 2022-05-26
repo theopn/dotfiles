340a89c9360916b731e8c9785c36d43c3ffc890b
theopn <63016528+theopn@users.noreply.github.com>
2022-05-25 23:59:19 -0400

    Installation is slightly better now

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,7 +1,14 @@
-sudo rm -rf ~/.gitignore > /dev/null 2>&1
-
-SYMLINKS=()
-ln -sf ~/dotfiles/gitignore ~/.gitignore
-SYMLINKS+=('.gitignore')
-
-echo ${SYMLINKS[@]}
+printf "This script will remove and replace dotfiles with ones in the repository.
+Do you wish to continue? Type 'yes': "
+read -r initial_input
+case $initial_input in  
+  yes)
+    sudo rm -rf ~/.gitignore > /dev/null 2>&1
+    SYMLINKS=()
+    ln -sf ~/dotfiles/gitignore ~/.gitignore
+    SYMLINKS+=('.gitignore')
+    echo ${SYMLINKS[@]} modified.
+    ;;
+  *) 
+    printf "Ending the script...";; 
+esac
