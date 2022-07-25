930bad24d49294af15eac276915f2d8945f398a8
theopn <63016528+theopn@users.noreply.github.com>
2022-07-25 15:45:22 -0400

    Awesome battery widget added

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -221,6 +221,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            require("battery-widget") {},
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
