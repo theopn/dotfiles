b951bfdca453b5bbb2d7520393284c10bfc7e5ca
theopn <63016528+theopn@users.noreply.github.com>
2022-08-14 10:41:15 -0400

    Neovim path completion

--- nvim/lua/theo_lsp.lua
+++ nvim/lua/theo_lsp.lua
@@ -107,9 +107,10 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    }, {
-      { name = 'buffer' },
-    }),
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
   formatting = {
     format = require("lspkind").cmp_format({ with_text = false, maxwidth = 50 })
   }
