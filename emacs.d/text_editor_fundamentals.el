e6ba749634f724cdff79bda3b35318f41bee8aaf
Theo P <theo.park.n@gmail.com>
2022-11-22 16:31:16 -0500

    (emacs) more bindings from vimrc

--- emacs.d/text_editor_fundamentals.el
+++ emacs.d/text_editor_fundamentals.el
@@ -21,12 +21,14 @@
 (setq-default java-indent-offset 4) ; Why Java
 ;; ------------------------------------
 
+;; Displaying tab and trailing whitespace
 (setq whitespace-style '(face tabs tab-mark trailing))
 (custom-set-faces
  '(whitespace-tab ((t (:foreground "#636363")))))
 (setq whitespace-display-mappings
   '((tab-mark 9 [124 9] [92 9]))) ; 124 is the ascii ID for '\|'
 (global-whitespace-mode) ; Enable whitespace mode everywhere
+;; --------------------------------------
 
 ;; Evil mode
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -39,15 +41,27 @@
   :config
   (evil-mode 1)
 
-  ;; More or less necessary bindings
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state) ; C-g in insert is ESC
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-backward-char) ; C-h in insert is left arrow
-
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
+  ;; Custom evil keybindings figure this by using <C-h> k <key u want>
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state) ; C-g in insert is ESC
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-backward-char) ; Sorry you are not getting help in insert mode
+  (define-key evil-insert-state-map (kbd "C-j") 'evil-next-visual-line)
+  (define-key evil-insert-state-map (kbd "C-k") 'evil-previous-visual-line)
+  (define-key evil-insert-state-map (kbd "C-l") 'evil-forward-char)
+  (define-key evil-normal-state-map (kbd "SPC h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "SPC j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "SPC k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "SPC l") 'evil-window-right)
+  (define-key evil-normal-state-map (kbd "SPC w") 'delete-window)
+
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
-;;
+
+(use-package evil-escape)
+(evil-escape-mode 1)
+(setq-default evil-escape-key-sequence "jk") ; Imagine requiring a separate package for this
+;; ---------
 
