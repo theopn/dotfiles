8ec6d150e87f4c8fe40f9dd7359a4c2b755f133d
Theo P <theo.park.n@gmail.com>
2023-02-24 12:57:23 -0500

    (rofi) powermenu revised and replaces i3 mode

--- rofi/rofi-powermenu.sh
+++ rofi/rofi-powermenu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#
 # Revised from https://github.com/adi1090x/rofi/tree/master/files/powermenu
+#
 
 # Options
 shutdown='  shutdown'
@@ -11,15 +13,41 @@ logout=' 󰗼 logout'
 yes='  yes'
 no=' 󰜺 no'
 
-# Rofi CMD
-function rofi_cmd() {
+# List of commands
+shutdown_cmd='systemctl poweroff'
+suspend_cmd='systemctl suspend'
+reboot_cmd='systemctl reboot'
+function lock() {
+  LOCK_SCREEN=~/dotfiles/assets/naomi-solarsys-draculafied-lockscreen.png
+
+  if [ -e $LOCK_SCREEN ]
+  then
+    i3lock -i $LOCK_SCREEN
+  else
+    i3lock -c "#282a36"
+  fi
+}
+
+function exit_wm() {
+  if [[ "$DESKTOP_SESSION" == "i3" ]]; then
+    i3-msg exit
+  fi
+}
+
+# Power menu window for rofi
+function rofi_selection_window() {
   rofi -dmenu \
     -p "Action for $(hostname)" \
     -mesg "Uptime: $(uptime -p | sed -e 's/up //g')"
 }
 
+# Pass variables to rofi dmenu
+function run_rofi_selection() {
+  echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_selection_window
+}
+
 # Confirmation CMD
-function confirm_cmd() {
+function rofi_confirmation_window() {
   rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
     -theme-str 'mainbox {children: [ "message", "listview" ];}' \
     -theme-str 'listview {columns: 2; lines: 1;}' \
@@ -31,29 +59,22 @@ function confirm_cmd() {
 }
 
 # Ask for confirmation
-function confirm_exit() {
-  echo -e "$yes\n$no" | confirm_cmd
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-  echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+function run_rofi_confirmation() {
+  echo -e "$yes\n$no" | rofi_confirmation_window
 }
 
 # Execute Command
-run_cmd() {
-  selected="$(confirm_exit)"
+function confirm_then_run() {
+  selected="$(run_rofi_confirmation)"
   if [[ "$selected" == "$yes" ]]; then
     if [[ $1 == "--shutdown" ]]; then
-      systemctl poweroff
+      $shutdown_cmd
     elif [[ $1 == "--reboot" ]]; then
-      systemctl reboot
+      $reboot_cmd
     elif [[ $1 == '--suspend' ]]; then
-      systemctl suspend
+      $suspend_cmd
     elif [[ $1 == "--logout" ]]; then
-      if [[ "$DESKTOP_SESSION" == "i3" ]]; then
-        i3-msg exit
-      fi
+      exit_wm
     fi
   else
     exit 0
@@ -62,22 +83,22 @@ run_cmd() {
 
 # Actions
 function main() {
-  chosen="$(run_rofi)"
+  chosen="$(run_rofi_selection)"
   case ${chosen} in
+    $lock)
+      lock # No confirmation needed for lock
+    ;;
     $shutdown)
-      run_cmd --shutdown
+      confirm_then_run --shutdown
     ;;
     $reboot)
-      run_cmd --reboot
-    ;;
-    $lock)
-      ~/dotfiles/i3/i3lock-launch.sh
+      confirm_then_run --reboot
     ;;
     $suspend)
-    run_cmd --suspend
+      confirm_then_run --suspend
     ;;
     $logout)
-    run_cmd --logout
+      confirm_then_run --logout
     ;;
   esac
 }
