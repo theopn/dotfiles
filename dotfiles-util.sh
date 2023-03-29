423d5398a2e68b4ae225e8b76bb34c456f604f57
Theo P <theo.park.n@gmail.com>
2023-03-29 00:03:54 -0400

    (script) curly brackets around the variables. Is it a good idea? I don't know

--- dotfiles-util.sh
+++ dotfiles-util.sh
@@ -8,54 +8,54 @@ DOT_BACKUP_DIR=~/dotfiles.bu
 # From https://github.com/vsbuffalo/dotfiles/blob/master/setup.sh
 # For regular messages
 function green_echo() {
-  echo -e "\033[0;32m[Message] $1\033[0m"
+  echo -e "\033[0;32m[Message] ${1}\033[0m"
 }
 
 # For prompts requiring user attention
 function yellow_echo() {
   echo
-  echo -e "\033[0;33m[Attention] $1\033[0m"
+  echo -e "\033[0;33m[Attention] ${1}\033[0m"
   echo
 }
 
 # For error prompts
 function red_echo() {
   echo
-  echo -e "\033[0;31m[Fatal] $1\033[0m"
+  echo -e "\033[0;31m[Fatal] ${1}\033[0m"
   echo
 }
 
 function verify_script_dir() {
-  script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-  if [[ "$script_dir" != "$DOT_DIR" || ! -d $DOT_DIR ]]; then
-    red_echo "$DOT_DIR directory not found"
+  script_dir=$( cd -- "$( dirname -- ${BASH_SOURCE[0]} )" &> /dev/null && pwd )
+  if [[ ${script_dir} != ${DOT_DIR} || ! -d ${DOT_DIR} ]]; then
+    red_echo "${DOT_DIR} directory not found"
     exit 1
   fi
 }
 
 # $1 = Name of the related files, $2 command to be prompt when user selected yes
 function selection_prompt() {
-  yellow_echo "Would you like to install $1 related files?"
+  yellow_echo "Would you like to install ${1} related files?"
   read -p "y/n? > " -n1 -r REPLY # -p for prompt, -n1 for reading 1 character, -r for reading literally
   echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    green_echo "Deploying $1 related files..."
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+    green_echo "Deploying ${1} related files..."
     true
   else
-    green_echo "Skipping $1 related files..."
+    green_echo "Skipping ${1} related files..."
     false
   fi
 }
 
 # $1 = Current location, $2 Target location
 function backup_then_symlink() {
-  if [[ -f $2 ]]; then
-    green_echo "Existing $2 will be moved to $DOT_BACKUP_DIR."
-    mkdir -p $DOT_BACKUP_DIR
-    mv $2 $DOT_BACKUP_DIR/
+  if [[ -f ${2} ]]; then
+    green_echo "Existing ${2} will be moved to ${DOT_BACKUP_DIR}."
+    mkdir -p ${DOT_BACKUP_DIR}
+    mv $2 ${DOT_BACKUP_DIR}/
   fi
-  green_echo "Creating symlink for $1 at $2..."
-  ln -s $1 $2
+  green_echo "Creating symlink for ${1} at ${2}..."
+  ln -s ${1} ${2}
 }
 
 ########## INSTALL FUNCTIONS ##########
@@ -71,71 +71,71 @@ function install() {
   verify_script_dir
 
   if selection_prompt 'Bash'; then # Do not use bracket around a function that returns command (which true/false are)
-    backup_then_symlink "$DOT_DIR"/bash/bashrc ~/.bashrc
+    backup_then_symlink ${DOT_DIR}/bash/bashrc ~/.bashrc
   fi
 
   if selection_prompt 'Doom'; then
     CURRENT_FILES=("init.el" "config.el" "packages.el")
     mkdir -p ~/.doom.d/
     for FILE in ${CURRENT_FILES[@]}; do
-      backup_then_symlink "$DOT_DIR"/doom.d/"$FILE" ~/.doom.d/"$FILE"
+      backup_then_symlink ${DOT_DIR}/doom.d/${FILE} ~/.doom.d/${FILE}
     done
   fi
 
   if selection_prompt 'Git'; then
     CURRENT_FILES=("gitignore_global" "gitconfig")
     for FILE in ${CURRENT_FILES[@]}; do
-      backup_then_symlink "$DOT_DIR"/git/"$FILE" ~/."$FILE"
+      backup_then_symlink ${DOT_DIR}/git/${FILE} ~/.${FILE}
     done
   fi
 
   if selection_prompt 'IdeaVim'; then
-    backup_then_symlink "$DOT_DIR"/ideavim/ideavimrc ~/.ideavimrc
+    backup_then_symlink ${DOT_DIR}/ideavim/ideavimrc ~/.ideavimrc
   fi
 
   if selection_prompt 'Kitty'; then
     mkdir -p ~/.config/kitty/
-    backup_then_symlink "$DOT_DIR"/kitty/kitty.conf ~/.config/kitty/kitty.conf
+    backup_then_symlink ${DOT_DIR}/kitty/kitty.conf ~/.config/kitty/kitty.conf
   fi
 
   if selection_prompt 'lf'; then
     CURRENT_FILES=('lfrc' 'icons')
     mkdir -p ~/.config/lf/
     for FILE in ${CURRENT_FILES[@]}; do
-      backup_then_symlink "$DOT_DIR"/lf/"$FILE" ~/.config/lf/"$FILE"
+      backup_then_symlink ${DOT_DIR}/lf/${FILE} ~/.config/lf/${FILE}
     done
   fi
 
   if selection_prompt 'Neofetch'; then
     mkdir -p ~/.config/neofetch/
-    backup_then_symlink "$DOT_DIR"/neofetch/config.conf ~/.config/neofetch/config.conf
+    backup_then_symlink ${DOT_DIR}/neofetch/config.conf ~/.config/neofetch/config.conf
   fi
 
   if selection_prompt 'Qutebrowser'; then
-    if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ "${OSTYPE}" == "darwin"* ]]; then
       mkdir -p ~/.qutebrowser/
-      backup_then_symlink "$DOT_DIR"/qutebrowser/config.py ~/.qutebrowser/config.py
-    elif [[ "$OSTYPE" == "linux-gnu"* ]];then
+      backup_then_symlink ${DOT_DIR}/qutebrowser/config.py ~/.qutebrowser/config.py
+    elif [[ "${OSTYPE}" == "linux-gnu"* ]];then
       mkdir -p ~/.config/qutebrowser/
-      backup_then_symlink "$DOT_DIR"/qutebrowser/config.py ~/.config/qutebrowser/config.py
+      backup_then_symlink ${DOT_DIR}/qutebrowser/config.py ~/.config/qutebrowser/config.py
     fi
   fi
 
   if selection_prompt 'Tmux'; then
-    backup_then_symlink "$DOT_DIR"/tmux/tmux.conf ~/.tmux.conf
+    backup_then_symlink ${DOT_DIR}/tmux/tmux.conf ~/.tmux.conf
   fi
 
   if selection_prompt 'Vim'; then
-    backup_then_symlink "$DOT_DIR"/vim/vimrc ~/.vimrc
+    backup_then_symlink ${DOT_DIR}/vim/vimrc ~/.vimrc
   fi
 
   if selection_prompt 'Vim Colorscheme'; then
     mkdir -p ~/.vim/
-    backup_then_symlink "$DOT_DIR"/vim/colors ~/.vim/colors
+    backup_then_symlink ${DOT_DIR}/vim/colors ~/.vim/colors
   fi
 
   if selection_prompt 'Zsh'; then
-    backup_then_symlink "$DOT_DIR"/zsh/zshrc ~/.zshrc
+    backup_then_symlink ${DOT_DIR}/zsh/zshrc ~/.zshrc
   fi
 
   yellow_echo 'Ending the dotfiles installation...'
@@ -156,8 +156,8 @@ function macos-install() {
 
   verify_script_dir
 
-  if [[ "$OSTYPE" != "darwin"* ]]; then
-    red_echo "Theo it seems like you are not rich enough to afford a MacBook. OSTYPE == $OSTYPE"
+  if [[ "${OSTYPE}" != "darwin"* ]]; then
+    red_echo "Theo it seems like you are not rich enough to afford a MacBook. OSTYPE == ${OSTYPE}"
     exit 1
   fi
 
@@ -168,18 +168,18 @@ function macos-install() {
   if selection_prompt 'Homebrew Core'; then
     #brew remove --force $(brew list --formula)
     #brew remove --cask --force $(brew list)
-    brew bundle --file "$DOT_DIR"/homebrew/Brewfile_core
+    brew bundle --file ${DOT_DIR}/homebrew/Brewfile_core
   fi
 
   if selection_prompt 'macOS Settings'; then
-    "${DOT_DIR}/macos/macos-settings.sh"
+    ${DOT_DIR}/macos/macos-settings.sh
   fi
 
   green_echo 'Homebrew Optional Formulae/Casks:
   This might take a while, and I honestly recommend you to go through each program manually.
   Do you want to proceed?'
   if selection_prompt 'Homebrew Optional'; then
-    brew bundle --file "$DOT_DIR"/homebrew/Brewfile_optional
+    brew bundle --file ${DOT_DIR}/homebrew/Brewfile_optional
   fi
 
   yellow_echo 'Ending the macos specific installation...'
@@ -190,34 +190,34 @@ function i3_install() {
 
   verify_script_dir
 
-  if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    red_echo "Theo you are not using Linux and therefore you are uncool. OSTYPE == $OSTYPE"
+  if [[ "${OSTYPE}" != "linux-gnu"* ]]; then
+    red_echo "Theo you are not using Linux and therefore you are uncool. OSTYPE == ${OSTYPE}"
     exit 1
   fi
 
   if selection_prompt 'i3'; then
     mkdir -p ~/.config/i3/
-    backup_then_symlink "$DOT_DIR"/i3/config ~/.config/i3/config
+    backup_then_symlink ${DOT_DIR}/i3/config ~/.config/i3/config
   fi
 
   yellow_echo 'Polybar will be launched from the dotfiles directory; no install needed'
 
   if selection_prompt 'dunst'; then
     mkdir -p ~/.config/dunst/
-    backup_then_symlink "$DOT_DIR"/dunst/dunstrc ~/.config/dunst/dunstrc
+    backup_then_symlink ${DOT_DIR}/dunst/dunstrc ~/.config/dunst/dunstrc
   fi
 
   if selection_prompt 'rofi'; then
     mkdir -p ~/.config/rofi/
-    backup_then_symlink "$DOT_DIR"/rofi/config.rasi ~/.config/rofi/config.rasi
+    backup_then_symlink ${DOT_DIR}/rofi/config.rasi ~/.config/rofi/config.rasi
   fi
 }
 
 ########## AUXILIARY FUNCTIONS ##########
 
 function delete_backup() {
-  yellow_echo "Deleting $DOT_BACKUP_DIR..."
-  rm -rf $DOT_BACKUP_DIR
+  yellow_echo "Deleting ${DOT_BACKUP_DIR}..."
+  rm -rf ${DOT_BACKUP_DIR}
 }
 
 function add_ssh_shortcut() {
@@ -229,22 +229,22 @@ function add_ssh_shortcut() {
   read host_url
   echo -n 'add_ssh_shortcut) Enter username for the host (e.g. good_student_69): '
   read username
-  echo "Host $host_nickname
-    Hostname $host_url
-    User $username" >> ~/.ssh/config
-  green_echo "${username}@${host_url} has been added to the SSH config! Try <ssh $host_nickname>."
+  echo "Host ${host_nickname}
+    Hostname ${host_url}
+    User ${username}" >> ~/.ssh/config
+  green_echo "${username}@${host_url} has been added to the SSH config! Try <ssh ${host_nickname}>."
 }
 
 function install_font() {
-  if [[ ! $2 ]]; then red_echo 'Target URL is missing!'; exit 1; fi
+  if [[ ! ${2} ]]; then red_echo 'Target URL is missing!'; exit 1; fi
   mkdir -p ~/.local/share/fonts
   cd ~/.local/share/fonts
-  wget -O temp-font.zip $1
+  wget -O temp-font.zip ${1}
   unzip temp-font.zip
   rm temp-font.zip
   fc-cache -vf
   cd - > /dev/null 2>&1
-  green_echo "A font from $1 successfully installed!"
+  green_echo "A font from ${1} successfully installed!"
 }
 
 function help() {
@@ -269,7 +269,7 @@ function help() {
 ########### MAIN CALL HERE ##########
 #
 function main() {
-  case $1 in
+  case ${1} in
     "--install")
       install
     ;;
@@ -286,7 +286,7 @@ function main() {
       add_ssh_shortcut
     ;;
     "--install-font")
-      install_font $2
+      install_font ${2}
     ;;
     "--help")
       help
@@ -300,7 +300,7 @@ function main() {
   exit 0
 }
 
-main $@
+main ${@}
 
 ########### MAIN CALL HERE ##########
 
