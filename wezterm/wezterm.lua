db25d8bfa15fa98328b5706d7438eadb0b909620
Theo P <theo.park.n@gmail.com>
2023-07-26 22:17:04 +0900

    feat(wezterm): add statusbar components, keybindings, and many things

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,93 +1,175 @@
+--[[ wezterm.lua
+-- $ figlet -f small Wezterm
+-- __      __      _
+-- \ \    / /__ __| |_ ___ _ _ _ __
+--  \ \/\/ / -_)_ /  _/ -_) '_| '  \
+--   \_/\_/\___/__|\__\___|_| |_|_|_|
+--
+-- My Wezterm config file
+--]]
 local wezterm = require("wezterm")
+local act = require("wezterm").action
 
 local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end
 
--- Appearance
-config.font = wezterm.font_with_fallback({
-  { family = "CaskaydiaCove Nerd Font",     scale = 1.2 },
-  { family = "FantasqueSansMono Nerd Font", scale = 1.2 },
-})
-config.color_scheme = "Dracula"
+-- {{{ Settings
+config.window_close_confirmation = "AlwaysPrompt"
+config.default_workspace = "home"
+-- }}}
 
-wezterm.on('update-right-status', function(window, pane)
-  -- Each element holds the text for a cell in a "powerline" style << fade
-  local cells = {}
-
-  -- Figure out the cwd and host of the current pane.
-  -- This will pick up the hostname for the remote host if your
-  -- shell is using OSC 7 on the remote host.
-  local cwd_uri = pane:get_current_working_dir()
-  if cwd_uri then
-    cwd_uri = cwd_uri:sub(8)
-    local slash = cwd_uri:find '/'
-    local cwd = ''
-    local hostname = ''
-    if slash then
-      hostname = cwd_uri:sub(1, slash - 1)
-      -- Remove the domain name portion of the hostname
-      local dot = hostname:find '[.]'
-      if dot then
-        hostname = hostname:sub(1, dot - 1)
-      end
-      -- and extract the cwd from the uri
-      cwd = cwd_uri:sub(slash)
-
-      table.insert(cells, cwd)
-      table.insert(cells, hostname)
-    end
+-- {{{ Colors & Appearance
+local schemes = {}
+for name, scheme in pairs(wezterm.get_builtin_color_schemes()) do
+  table.insert(schemes, name)
+end
+local scheme = schemes[math.random(#schemes)]
+wezterm.on("window-config-reloaded", function(window, pane)
+  if not window:get_config_overrides() then
+    window:set_config_overrides { color_scheme = scheme, }
   end
-
-  -- I like my date/time in this style: "Wed Mar 3 08:14"
-  local date = wezterm.strftime '%a %b %-d %H:%M'
-  table.insert(cells, date)
-
-  -- An entry for each battery (typically 0 or 1 battery)
-  for _, b in ipairs(wezterm.battery_info()) do
-    table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
-  end
-
-  -- The powerline < symbol
-  local LEFT_ARROW = utf8.char(0xe0b3)
-  -- The filled in variant of the < symbol
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-
-  -- Color palette for the backgrounds of each cell
-  local colors = {
-    '#3c1361',
-    '#52307c',
-    '#663a82',
-    '#7c5295',
-    '#b491c8',
-  }
-
-  -- Foreground color for the text across the fade
-  local text_fg = '#c0c0c0'
-
-  -- The elements to be formatted
-  local elements = {}
-  -- How many cells have been formatted
-  local num_cells = 0
-
-  -- Translate a cell into elements
-  local function push(text, is_last)
-    local cell_no = num_cells + 1
-    table.insert(elements, { Foreground = { Color = text_fg } })
-    table.insert(elements, { Background = { Color = colors[cell_no] } })
-    table.insert(elements, { Text = ' ' .. text .. ' ' })
-    if not is_last then
-      table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
-      table.insert(elements, { Text = SOLID_LEFT_ARROW })
-    end
-    num_cells = num_cells + 1
-  end
-
-  while #cells > 0 do
-    local cell = table.remove(cells, 1)
-    push(cell, #cells == 0)
-  end
-
-  window:set_right_status(wezterm.format(elements))
 end)
+
+config.colors = {
+  tab_bar = {
+    background = "#0B0022",
+    active_tab = {
+      bg_color = "#2B2042",
+      fg_color = "#C0C0C0",
+      italic = false,
+    },
+    inactive_tab = {
+      bg_color = "#1B1032",
+      fg_color = "#808080",
+      italic = false,
+    },
+    inactive_tab_hover = {
+      bg_color = "#3B3052",
+      fg_color = "#909090",
+      italic = true,
+    },
+  },
+}
+
+config.font = wezterm.font_with_fallback({
+  { family = "CaskaydiaCove Nerd Font",     scale = 1.24 },
+  { family = "FantasqueSansMono Nerd Font", scale = 1.24 },
+})
+
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5,
+}
+-- }}}
+
+-- {{{ Tab bar
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = true
+config.show_new_tab_button_in_tab_bar = false
+
+config.status_update_interval = 1000
+wezterm.on("update-right-status", function(window, pane)
+  -- Workspace name
+  local ws = window:active_workspace()
+  -- Status display
+  local status = "N"
+  local name = window:active_key_table()
+  if name then status = name end
+  if window:leader_is_active() then status = "LDR" end
+
+  -- cwd
+  local cwd = pane:get_current_working_dir()
+  cwd = cwd:sub(cwd:match("^.*()/")) --> or /[^/]*$
+
+  -- Current command
+  local cmd = pane:get_title()
+
+  -- time
+  local time = wezterm.strftime(" %H:%M")
+
+  -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+  window:set_right_status(wezterm.format({
+    { Foreground = { AnsiColor = "Teal" } },
+    { Text = " " },
+    "ResetAttributes",
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. ws },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_folder .. " " .. cwd },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.fa_code .. " " .. cmd },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_creation .. " Mode: " .. status },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock_alert .. time },
+    "ResetAttributes",
+    { Foreground = { AnsiColor = "Teal" } },
+    { Text = " " },
+  }))
+end)
+
+
+-- }}}
+
+-- {{{ Launching Programs
+--config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+-- }}}
+
+-- {{{ Keys
+-- USe `wezterm show-keys --lua` to get alll thedefault keybindings
+config.disable_default_key_bindings = false
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+  { key = "a", mods = "LEADER|CTRL",  action = act.SendKey { key = "a", mods = "CTRL" }, },
+  { key = "[", mods = "LEADER",       action = act.ActivateCopyMode, },
+  -- Pane bindings
+  { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left"), },
+  { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down"), },
+  { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up"), },
+  { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right"), },
+  { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" }, },
+  { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" }, },
+  { key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+  { key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false, }, },
+  -- Tab bindings
+  { key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
+  { key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain"), },
+  { key = "w", mods = "CMD",          action = wezterm.action.CloseCurrentTab { confirm = true }, },
+  {
+    key = ",",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
+  { key = "u", mods = "LEADER", action = act.SpawnCommandInNewTab { args = { "top" }, } },
+  -- Workspace bindings
+  { key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES", }, },
+  { key = "?", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|KEY_ASSIGNMENTS", }, },
+}
+
+config.key_tables = {
+  resize_pane = {
+    { key = 'h',      action = act.AdjustPaneSize { 'Left', 1 } },
+    { key = 'l',      action = act.AdjustPaneSize { 'Right', 1 } },
+    { key = 'k',      action = act.AdjustPaneSize { 'Up', 1 } },
+    { key = 'j',      action = act.AdjustPaneSize { 'Down', 1 } },
+    { key = 'Enter',  action = 'PopKeyTable' },
+    { key = 'Escape', action = 'PopKeyTable' },
+  }
+}
+-- }}}
 
 return config
