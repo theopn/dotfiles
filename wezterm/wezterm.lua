964d6ba597789013435f0e1e538fff99463381d1
Theo P <theo.park.n@gmail.com>
2023-08-01 15:53:57 +0900

    feat(wezterm): add tab renaming binding and fish as default shell

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,12 @@
+--[[ wezterm.lua
+-- $ figlet -f small Wezterm
+-- __      __      _
+-- \ \    / /__ __| |_ ___ _ _ _ __
+--  \ \/\/ / -_)_ /  _/ -_) '_| '  \
+--   \_/\_/\___/__|\__\___|_| |_|_|_|
+--
+-- My Wezterm config file
+--]]
 local wezterm = require("wezterm")
 local act = wezterm.action
 
@@ -6,6 +15,8 @@ local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- Settings
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font_with_fallback({
   { family = "CaskaydiaCove Nerd Font",  scale = 1.2 },
@@ -27,35 +38,52 @@ config.inactive_pane_hsb = {
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
   -- Send C-a when pressing C-a twice
-  { key = "a", mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "c", mods = "LEADER",       action = act.ActivateCopyMode },
+  { key = "a",          mods = "LEADER",       action = act.SendKey { key = "a", mods = "CTRL" } },
+  { key = "c",          mods = "LEADER",       action = act.ActivateCopyMode },
+  { key = "phys:Space", mods = "LEADER",       action = act.ActivateCommandPalette },
 
   -- Pane keybindings
-  { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "-",          mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
   -- SHIFT is for when caps lock is on
-  { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-  { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-  { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-  { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
-  { key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
-  { key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
-  { key = "s", mods = "LEADER",       action = act.RotatePanes "Clockwise" },
+  { key = "|",          mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h",          mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+  { key = "j",          mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+  { key = "k",          mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+  { key = "l",          mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+  { key = "x",          mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+  { key = "z",          mods = "LEADER",       action = act.TogglePaneZoomState },
+  { key = "s",          mods = "LEADER",       action = act.RotatePanes "Clockwise" },
   -- We can make separate keybindings for resizing panes
   -- But Wezterm offers custom "mode" in the name of "KeyTable"
-  { key = "r", mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+  { key = "r",          mods = "LEADER",       action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
 
   -- Tab keybindings
-  { key = "n", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "[", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
-  { key = "]", mods = "LEADER",       action = act.ActivateTabRelative(1) },
-  { key = "t", mods = "LEADER",       action = act.ShowTabNavigator },
-  -- Key table for moving tabs around
-  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+  { key = "n",          mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "[",          mods = "LEADER",       action = act.ActivateTabRelative(-1) },
+  { key = "]",          mods = "LEADER",       action = act.ActivateTabRelative(1) },
+  { key = "t",          mods = "LEADER",       action = act.ShowTabNavigator },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
 
+  -- Key table for moving tabs around
+  { key = "m", mods = "LEADER", action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
 
   -- Lastly, workspace
-  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+  { key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
 
 }
 -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
