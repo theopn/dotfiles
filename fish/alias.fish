3f2348de8304b05cc750a6930cc0bf3e6d2b95f1
Theo P <theo.park.n@gmail.com>
2023-08-23 18:01:23 +0900

    feat(fish): change cdf alias to use fzf and rename old cdf alias to fav

--- fish/alias.fish
+++ fish/alias.fish
@@ -5,13 +5,24 @@
 #
 # Functions and aliases (which are just a wrapper for functions in Fish)
 
-alias cl="clear"
-alias l="ls -Alh" # [A]lmost all (except . && ..), [l]ist, [h]: display unit
-alias weather="curl 'https://wttr.in'"
+alias cdf 'cd $(find * -maxdepth 1 -type d | fzf)'
+alias cl "clear"
+alias l "ls -Alh" # [A]lmost all (except . && ..), [l]ist, [h]: display unit
+alias weather "curl 'https://wttr.in'"
 
 alias l1="cd \"$CACHE_DIR\""
 alias dw="vim \"$DAILY_WRITING_DIR/index.md\""
 alias dot="cd \"$DOT_DIR\""
+
+function fav -d "Navigate to my favorite directories using fzf"
+  set -l fav_dir "$CLOUD_DIR" "$CACHE_DIR" "$DOT_DIR"
+  set selected $(printf "%s\n" $fav_dir | fzf --reverse --border=rounded --cycle --height=30% --header='Pick a directory to navigate to')
+  if [ -z "$selected" ]
+    echo "Nothing was selected :("
+    return 1
+  end
+  cd "$selected"
+end
 
 function mkcd -d "Create a directory and set CWD"
   command mkdir $argv
@@ -31,17 +42,12 @@ function numfiles -d "Count the number of file in the directory"
   [ -z $num ] && echo "$num files in $argv"
 end
 
-function cdf -d "Navigate to my favorite directories using fzf"
-  set -l fav_dir "$CLOUD_DIR" "$CACHE_DIR" "$DOT_DIR"
-  set selected $(printf "%s\n" $fav_dir | fzf --reverse --border=rounded --cycle --height=30% --header='Pick a directory to navigate to')
-  if [ -z "$selected" ]
-    echo "Nothing was selected :("
-    return 1
-  end
-  cd "$selected"
-end
-
 function updater -d "Place to update all the different stuff"
   echo "what's up"
+end
+
+
+function note -d "Quick note"
+  
 end
 
