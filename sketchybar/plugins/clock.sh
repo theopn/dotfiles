d1295567a842cde06ac3c33555840b78724d1121
Theo P <theo.park.n@gmail.com>
2024-02-26 19:30:08 +0900

    fix(sketchybar): change font and update the default plugins based on https://github.com/FelixKratz/SketchyBar/commit/c6b8aa8288e4895f0d7764fb28376ef90cc00740

--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -4,5 +4,4 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set $NAME label="$(date '+%a %m-%d %H:%M')"
-
+sketchybar --set "$NAME" label="$(date '+%a %m-%d %H:%M')"
--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -4,17 +4,17 @@
 # percentage is passed to the script.
 
 if [ "$SENDER" = "volume_change" ]; then
-  VOLUME=$INFO
+  VOLUME="$INFO"
 
-  case $VOLUME in
-    [6-9][0-9]|100) ICON="󰕾 "
+  case "$VOLUME" in
+    [6-9][0-9]|100) ICON="󰕾"
     ;;
-    [3-5][0-9]) ICON="󰖀 "
+    [3-5][0-9]) ICON="󰖀"
     ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿 "
+    [1-9]|[1-2][0-9]) ICON="󰕿"
     ;;
-    *) ICON="󰝟 "
+    *) ICON="󰖁"
   esac
 
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
 fi
