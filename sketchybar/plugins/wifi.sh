23117053b73a59c6bde30f4e56f05944f206d2fa
Theo P <theo.park.n@gmail.com>
2024-02-26 19:37:05 +0900

    fix(skechybar): add a temporary workaround for broken SSID information in macOS Sonoma (https://github.com/FelixKratz/SketchyBar/issues/407#issuecomment-1755765318)

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,6 +3,10 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 
+# Workaround for Apple stop supplying the SSID after the Sonoma update
+# https://github.com/FelixKratz/SketchyBar/issues/407#issuecomment-1755765318
+INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')"
+
 if [ "$SENDER" = "wifi_change" ]; then
   WIFI=${INFO:-"Not Connected"}
   sketchybar --set $NAME label="${WIFI}"
