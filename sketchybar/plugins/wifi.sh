03c763e4209dc5be314a969ee5597f6a30131f7b
Theo P <theo.park.n@gmail.com>
2024-10-23 20:58:36 +0900

    fix(skechybar): display ip address instead of ssid

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# The wifi_change event supplies a $INFO variable in which the current SSID
-# is passed to the script.
+# Wifi module breaks pretty much every macOS update, so let's settle with IP Address
+# macOS 14 Sonoma: https://github.com/FelixKratz/SketchyBar/issues/407
+# macOS 15 Sequoia: https://github.com/FelixKratz/SketchyBar/issues/517
+#INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
+#
+#if [ "$SENDER" = "wifi_change" ]; then
+#  WIFI=${INFO:-"Not Connected"}
+#  sketchybar --set $NAME label="${WIFI}"
+#fi
 
-# Workaround for Apple stop supplying the SSID after the Sonoma update
-# https://github.com/FelixKratz/SketchyBar/issues/407#issuecomment-1755765318
-INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')"
+INFO="$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)"
 
 if [ "$SENDER" = "wifi_change" ]; then
   WIFI=${INFO:-"Not Connected"}
