b8a6a0570b00b2ceaed5562b753d11bb5b443a85
Theo P <theo.park.n@gmail.com>
2025-03-17 12:58:33 +0900

    feat(nvim): add nvim-org-mode config with personal org settings and commonly used Emacs Org keybindings

---
+++ nvim/.config/nvim/lua/plugins/org-mode.lua
@@ -0,0 +1,53 @@
+-- Default config reference:
+-- https://github.com/nvim-orgmode/orgmode/blob/master/lua/orgmode/config/defaults.lua
+
+local caos_dir = "~/My Drive/l1-cache/caos/"
+
+return {
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  ft = { "org" },
+  config = function()
+    -- Setup orgmode
+    require("orgmode").setup({
+      org_agenda_files = { caos_dir .. "projects/active/*", caos_dir .. "capture/refile.org" },
+      org_default_notes_file = caos_dir .. "capture/refile.org",
+
+      org_todo_keywords = { "TODO", "|", "DONE" },
+      org_deadline_warning_days = 3,
+
+      org_capture_templates = {
+        t = {
+          description = "Task",
+          template = "* TODO %?\n  %u",
+        },
+        c = {
+          description = "Capture Idea",
+          template = "* "
+        }
+      },
+
+      org_agenda_skip_scheduled_if_done = true,
+      org_agenda_skip_deadline_if_done = true,
+
+      mappings = {
+        agenda = {
+          org_agenda_later = { "f", "]" },
+          org_agenda_earlier = { "b", "[" },
+        },
+        capture = {
+          org_capture_finalize = { "<C-c>", "<C-c><C-c>" },
+          org_capture_kill = { "<prefix>k", "<C-c><C-k>" },
+        },
+        org = {
+          org_priority = { "<C-c>,", "<prefix>," },
+          org_todo = { "cit", "<S-RIGHT>" },
+          org_todo_prev = { "ciT", "<S-LEFT>" },
+          org_insert_todo_heading_respect_content = { "<prefix>it", "<C-CR>" }, -- Add new todo heading after current heading block (same level)
+          org_deadline = { "<prefix>id", "<C-c><C-d>" },
+          org_schedule = { "<prefix>is", "<C-c><C-s>" },
+        },
+      }
+    })
+  end,
+}
