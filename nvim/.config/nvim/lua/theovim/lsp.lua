63e2e7a82fa556d3d1a1397a0ec91aaf4c82742c
Theo P <theo.park.n@gmail.com>
2025-03-27 17:07:23 +0900

    feat(nvim): add fzf features and K binding to LSP config

--- nvim/.config/nvim/lua/theovim/lsp.lua
+++ nvim/.config/nvim/lua/theovim/lsp.lua
@@ -2,6 +2,8 @@ local servers = {
   "bashls",
   "clangd",
   "luals",
+  "pylsp",
+  "texlab",
 }
 
 --vim.lsp.config()
@@ -13,8 +15,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local map = function(keys, func, desc)
       vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
     end
-    -- Creates a keybinding to format code
+
+    map("K", function()
+      vim.lsp.buf.hover({
+        border = "rounded",
+      })
+    end, "LSP Hover with rounded look")
+
+    map("<leader>q", vim.diagnostic.setloclist, "Open diagnostic [Q]uickfix list")
+
     map("<leader>f", vim.lsp.buf.format, "[F]ormat buffer")
+
+    -- Override default keybinding
+    local status, fzf = pcall(require, "fzf-lua")
+    if status then
+      map("gra", fzf.lsp_code_actions, "gr Code [A]ctions")
+      map("grr", fzf.lsp_references, "gr [R]eferences")
+      map("gri", fzf.lsp_implementations, "gr [I]mplementation")
+      map("gO", fzf.lsp_document_symbols, "g Document Symbols")
+      map("<leader>sd", fzf.diagnostics_document, "[S]earch [D]iagnostics")
+    end
   end,
 })
 
