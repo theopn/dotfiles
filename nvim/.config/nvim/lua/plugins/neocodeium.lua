4161e2e716844d6d12f867104fc5a017d09c6993
Theo P <theo.park.n@gmail.com>
2025-04-09 12:11:22 +0900

    refactor(nvim): revert back to neocodeium and add copilot example in the config

--- nvim/.config/nvim/lua/plugins/neocodeium.lua
+++ nvim/.config/nvim/lua/plugins/neocodeium.lua
@@ -1,6 +1,6 @@
 return {
   "monkoose/neocodeium",
-  enabled = false,
+  enabled = true,
   event = "VeryLazy",
   config = function()
     local neocodeium = require("neocodeium")
@@ -42,3 +42,31 @@ return {
     map_neocodeium("<C-e>", neocodeium.clear)
   end,
 }
+
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   enabled = true,
+--   cmd = "Copilot",
+--   event = "InsertEnter",
+--   config = function()
+--     local copilot = require("copilot")
+--     copilot.setup({ })
+--
+--     local suggestion = require("copilot.suggestion")
+--     vim.keymap.set("i", "<C-x><C-r>", suggestion.next)
+--     local function map_copilot(key, action)
+--       vim.keymap.set("i", key, function()
+--         if suggestion.is_visible() then
+--           return action()
+--         else
+--           return key
+--         end
+--       end, { expr = true })
+--     end
+--
+--     map_copilot("<C-n>", suggestion.next)
+--     map_copilot("<C-p>", suggestion.prev)
+--     map_copilot("<C-y>", suggestion.accept)
+--     map_copilot("<C-e>", suggestion.dismiss)
+--   end,
+-- }
