535da2eb88435c66cdfaf80f3b902d981fe44b1f
Theo P <theo.park.n@gmail.com>
2025-04-11 00:29:55 +0900

    refactor(nvim): switch to the built-in snippet engine and clean up nvim-cmp config for the last time

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -5,36 +5,26 @@ M.event = { "InsertEnter", "CmdlineEnter", }
 M.enabled = false
 
 M.dependencies = {
+  "hrsh7th/cmp-nvim-lsp",         --> nvim-cmp source for LSP engine
+  "hrsh7th/cmp-buffer",           --> nvim-cmp source for buffer words
+  "hrsh7th/cmp-cmdline",          --> nvim-cmp source for :commands
+  "rafamadriz/friendly-snippets", --> Snippet collections
   {
-    "L3MON4D3/LuaSnip", --> VS Code style snippet enginew
-    build = (function()
-      -- Build jsregexp unless in Windows or make is not available
-      if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-        return
-      end
-      return "make install_jsregexp"
-    end)(),
-    dependencies = {
-      "rafamadriz/friendly-snippets", --> Snippet collections
-    },
+    "garymjr/nvim-snippets",      --> plugin for expanding friendly_snippets
+    opts = {
+      friendly_snippets = true,
+      create_cmp_source = true,
+    }
   },
-  "saadparwaiz1/cmp_luasnip", --> Providing Luasnip as one of nvim-cmp source
-  "hrsh7th/cmp-nvim-lsp",     --> nvim-cmp source for LSP engine
-  "hrsh7th/cmp-buffer",       --> nvim-cmp source for buffer words
-  "hrsh7th/cmp-cmdline",      --> nvim-cmp source for :commands
 }
 
 M.config = function()
   local cmp = require("cmp")
-  local luasnip = require("luasnip")
-
-  require("luasnip.loaders.from_vscode").lazy_load()
-  luasnip.config.setup({})
 
   cmp.setup({
     snippet = {
       expand = function(args)
-        luasnip.lsp_expand(args.body)
+        vim.snippet.expand(args.body)
       end,
     },
 
@@ -45,20 +35,14 @@ M.config = function()
     },
 
     mapping = cmp.mapping.preset.insert({
-      -- Select the [n]ext item
       ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-j>"] = cmp.mapping.select_next_item(),
-      -- Select the [p]revious item
       ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-k>"] = cmp.mapping.select_prev_item(),
-
       ["<C-e>"] = cmp.mapping.abort(),
 
       -- Scoll the doc [b]ack / [f]orward
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-      -- Accept ([y]es or <CR>) the completion
       ["<C-y>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Replace,
         select = false,
@@ -71,27 +55,15 @@ M.config = function()
       -- Manually trigger the completion
       ["<C-Space>"] = cmp.mapping.complete({}),
 
-      -- Moving through the Luasnip expansion
-      ["<C-l>"] = cmp.mapping(function()
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        end
-      end, { "i", "s" }),
-      ["<C-h>"] = cmp.mapping(function()
-        if luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
-        end
-      end, { "i", "s" }),
-
       -- Tab completion
       -- Cycle through the completion items if completion menu is visible
-      -- Cycle through the Luasnip entries if applicable
+      -- Cycle through the snippet entries if applicable
       -- Else insert <TAB>
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump() --> use tab to jump completed function param
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.snippet.jump(1)
         else
           fallback()
         end
@@ -99,8 +71,8 @@ M.config = function()
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
+        elseif vim.snippet.active({ direction = -1 }) then
+          vim.snippet.jump(-1)
         else
           fallback()
         end
@@ -108,16 +80,11 @@ M.config = function()
 
     }),
 
-    sources = cmp.config.sources(
-      {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      },
-      {
-        { name = "buffer" },
-      }
-    ),
-
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "snippets" },
+      { name = "buffer" },
+    }),
   })
 
   -- cmp-cmdline setup
@@ -133,8 +100,6 @@ M.config = function()
   cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
       { name = 'cmdline' }
     })
   })
