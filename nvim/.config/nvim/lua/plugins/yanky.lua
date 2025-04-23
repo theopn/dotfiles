10cf24b03155b9fb97b752ffd814be59bc11cfb9
Theo P <theo.park.n@gmail.com>
2025-04-23 11:32:19 +0900

    feat(nvim): add yanky.nvim

---
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,24 @@
+return {
+  "gbprod/yanky.nvim",
+  opts = {
+    ring = {
+      history_length = 100,
+    },
+    highlight = {
+      on_put = true,
+      on_yank = true,
+      timer = 150,
+    },
+  },
+  keys = {
+    { "<leader>p", "<cmd>YankyRingHistory<cr>",              mode = { "n", "x" },                                desc = "Open Yank History" },
+    -- { "y",         "<Plug>(YankyYank)",                      mode = { "n", "x" },                                desc = "Yank text" },
+    { "p",         "<Plug>(YankyPutAfter)",                  mode = { "n", "x" },                                desc = "Put yanked text after cursor" },
+    { "P",         "<Plug>(YankyPutBefore)",                 mode = { "n", "x" },                                desc = "Put yanked text before cursor" },
+    { "gp",        "<Plug>(YankyGPutAfter)",                 mode = { "n", "x" },                                desc = "Put yanked text after selection" },
+    { "gP",        "<Plug>(YankyGPutBefore)",                mode = { "n", "x" },                                desc = "Put yanked text before selection" },
+    { "<c-p>",     "<Plug>(YankyPreviousEntry)",             desc = "Select previous entry through yank history" },
+    { "<c-n>",     "<Plug>(YankyNextEntry)",                 desc = "Select next entry through yank history" },
+  },
+}
+
