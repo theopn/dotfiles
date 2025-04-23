9ba66ea43d8f35468f9441b3b4144763836c4dcc
Theo P <theo.park.n@gmail.com>
2025-04-23 12:54:18 +0900

    fix(nvim): fix treesitter installation issue by disabling auto install

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,12 +9,27 @@ local M = { "nvim-treesitter/nvim-treesitter" }
 
 M.build = ":TSUpdate"
 
+M.main = "nvim-treesitter.configs"
+
 M.opts = {
-  ensure_installed = { "bash", "c", "cpp", "latex", "lua", "markdown", "python", },
+  --ensure_installed = { "bash", "c", "cpp", "latex", "lua", "markdown", "python", },
   auto_install = false,
 
-  highlight = { enable = true, },
-  indent = { enable = true },
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
+
+  indent = {
+    enable = true
+  },
+
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -25,9 +40,5 @@ M.opts = {
     },
   },
 }
-
-M.config = function(_, opts)
-  require("nvim-treesitter.configs").setup(opts)
-end
 
 return M
