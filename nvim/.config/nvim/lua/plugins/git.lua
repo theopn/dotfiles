506386fc9ad2c7c03cc7828495656dc0602dc4bf
Theo P <theo.park.n@gmail.com>
2025-04-26 16:58:54 +0900

    feat(nvim): add hunk navigation and git blame to gitsigns config

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,9 +8,40 @@ return {
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
     },
+
     on_attach = function(bufnr)
-      vim.keymap.set("n", "<leader>gd", require("gitsigns").diffthis,
-        { buffer = bufnr, desc = "[G]it [D]iff current buffer" })
+      local gitsigns = require("gitsigns")
+
+      local function map(mode, l, r, opts)
+        opts = opts or { desc = "" }
+        opts.buffer = bufnr
+        opts.desc = "[G]it " .. opts.desc
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end, { desc = "Next hunk" })
+
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end, { desc = "Previous hunk" })
+
+      -- Actions
+      map('n', '<leader>gd', gitsigns.diffthis, { desc = "[D]iff current buffer" })
+
+      map('n', '<leader>gb', function()
+        gitsigns.blame_line({ full = true })
+      end, { desc = "[B]lame" })
     end
   },
 }
