4ce9701eb5b0668ad92d92c69aea37d54148504b
Theo P <theo.park.n@gmail.com>
2025-05-10 17:00:03 +0900

    refactor(nvim): move Git and LSP modules to Winbar

--- nvim/.config/nvim/lua/ui/winbar.lua
+++ nvim/.config/nvim/lua/ui/winbar.lua
@@ -12,6 +12,7 @@ local function isOnlyWin()
   return #vim.api.nvim_tabpage_list_wins(0) == 1
 end
 
+
 local function getFileicon()
   if not Winbar.has_devicons then return "" end
 
@@ -27,19 +28,61 @@ local function getLspServers()
   if rawequal(next(clients), nil) then return "" end
 
   local format = vim.g.have_nerd_font and "󰧑" or "LSP:"
-  for _, client in ipairs(clients) do
-    format = string.format("%s [%s]", format, client.name)
+  for _, client in pairs(clients) do
+    format = string.format(" %s [%s]", format, client.name)
   end
   return format
 end
 
+
+local function getDiagnostics()
+  local str = ""
+
+  local levels = {
+    e = vim.diagnostic.severity.ERROR,
+    w = vim.diagnostic.severity.WARN,
+    i = vim.diagnostic.severity.INFO,
+    h = vim.diagnostic.severity.HINT,
+  }
+
+  local counts = {}
+  for level, id in pairs(levels) do
+    counts[level] = #vim.diagnostic.get(0, { severity = id })
+  end
+
+  for level, count in pairs(counts) do
+    if count > 0 then
+      str = string.format("%s %s: %i", str, level:upper(), count)
+    end
+  end
+
+  return str
+end
+
+
+local function getGitSigns()
+  if not vim.b.gitsigns_head then
+    return ""
+  end
+
+  local icon = vim.g.have_nerd_font and "" or "GIT:"
+  local head = vim.b.gitsigns_head or "-"
+  local status = vim.b.gitsigns_status or ""
+
+  return string.format(" %s [%s] %s", icon, head, status)
+end
+
+
 Winbar.build = function(isActive)
-  local highlight = isActive and "%#MiniStatuslineFileinfo#" or "%#WinbarNC#"
+  local highlight = function(hl)
+    return isActive and hl or "%#WinbarNC#"
+  end
 
   local winbar = table.concat({
     "%#Normal#",
-    highlight,
+    " ",
 
+    highlight("%#MiniStatuslineFilename#"),
     getFileicon(),
     "%<", --> Truncation starts here so the icon will be visible at all time
     " ",
@@ -49,17 +92,25 @@ Winbar.build = function(isActive)
     "%m",
     "%r ",
 
-    -- LSP Servers
+    highlight("%#MiniStatuslineDevinfo#"),
+    -- Git
+    getGitSigns(),
+
+    -- Spacer
+    "%=",
+
+    -- LSP info
+    highlight("%#MiniStatuslineFilename#"),
     getLspServers(),
+    getDiagnostics(),
 
-    "%=", --> spacer
-
-    " %P ",
-    "%#Normal#"
+    "%#Normal#",
+    " "
   })
 
   return winbar
 end
+
 
 function Winbar.setup()
   Winbar.has_devicons, Winbar.devicons = pcall(require, "nvim-web-devicons")
@@ -77,7 +128,7 @@ function Winbar.setup()
       callback = vim.schedule_wrap(function()
         vim.cmd("redrawstatus")
       end),
-      desc = "Update statusline/winbar"
+      desc = "Update statusline/winbar on LSP and diagnostics update"
     })
 end
 
