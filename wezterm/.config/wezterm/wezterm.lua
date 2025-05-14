0e1d81b10baf953c5b8dc9d4288d3f379cc58561
Theo P <theo.park.n@gmail.com>
2025-05-14 13:52:33 +0900

    refactor(wezterm): create a local table for color palette, change default colorscheme to nordfox, add macOS blur

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,11 +19,20 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 -- Settings
 config.default_prog = { fish_path, "-l" }
 
-config.color_scheme = "Tokyo Night"
+--config.color_scheme = "Tokyo Night"
+config.color_scheme = "nordfox"
+local custom_colors = {
+  red = "#D06F79",     --> Tokyonight: "#F7768e"
+  cyan = "#88C0D0",    --> Tokyonight: "#7DCFFF"
+  magenta = "#B48EAD", --> Tokyonight: "#BB9AF7"
+  yellow = "#EBCB8B",  --> Tokyonight: "#E0AF68"
+
+}
 config.font = wezterm.font_with_fallback({
   { family = "UbuntuMono Nerd Font", scale = 1.35, },
 })
 config.window_background_opacity = 0.9
+config.macos_window_background_blur = 24
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
@@ -131,16 +140,16 @@ config.tab_bar_at_bottom = true
 wezterm.on("update-status", function(window, pane)
   -- Workspace name
   local stat = window:active_workspace()
-  local stat_color = "#f7768e"
+  local stat_color = custom_colors.red
   -- It's a little silly to have workspace name all the time
   -- Utilize this to display LDR or current key table name
   if window:active_key_table() then
     stat = window:active_key_table()
-    stat_color = "#7dcfff"
+    stat_color = custom_colors.cyan
   end
   if window:leader_is_active() then
     stat = "LDR"
-    stat_color = "#bb9af7"
+    stat_color = custom_colors.magenta
   end
 
   local basename = function(s)
@@ -151,7 +160,7 @@ wezterm.on("update-status", function(window, pane)
   -- Current working directory
   local cwd = pane:get_current_working_dir()
   if cwd then
-    cwd = basename(cwd.file_path)  --> URL object introduced in 20240127-113634-bbcac864 (type(cwd) == "userdata")
+    cwd = basename(cwd.file_path) --> URL object introduced in 20240127-113634-bbcac864 (type(cwd) == "userdata")
     -- cwd = basename(cwd) --> 20230712-072601-f4abf8fd or earlier version
   else
     cwd = ""
@@ -176,7 +185,7 @@ wezterm.on("update-status", function(window, pane)
     -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
     { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
     { Text = " | " },
-    { Foreground = { Color = "#e0af68" } },
+    { Foreground = { Color = custom_colors.yellow } },
     { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
     { Text = "  " },
   }))
