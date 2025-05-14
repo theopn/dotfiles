0d2a9e297ad07b0f0ae6ca52918f34afe996797c
Theo P <theo.park.n@gmail.com>
2025-05-14 17:00:13 +0900

    feat(sketchybar): add and change to nordfox theme

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# Tokyonight (night) color scheme for Sketchybar
 # Use https://felixkratz.github.io/SketchyBar/config/tricks#color-picker
 
 export COLOR_TRANSPARENT="0x00000000"
 
-export COLOR_FOREGROUND="0xffa9b1dc" #"#a9b1dc"
-export COLOR_BACKGROUND="0xb0001a1b" #"#1a1b2c" but 69% transparent
+# Tokyonight (night)
+# export COLOR_FOREGROUND="0xffa9b1dc" #"#a9b1dc"
+# export COLOR_BACKGROUND="0xb0001a1b" #"#1a1b2c" but 69% transparent
+#
+# export COLOR_BLACK="0xff414868" #"#414868"
+# export COLOR_RED="0xfff7768e" #f7768e"
+# export COLOR_GREEN="0xff73daca" #"#73daca"
+# export COLOR_YELLOW="0xffe0af68" #"#e0af68"
+# export COLOR_BLUE="0xff7aa2f7" #"#7aa2f7"
+# export COLOR_PURPLE="0xffbb9af7" #"#bb9af7"
+# export COLOR_CYAN="0xff7dcfff" #"#7dcfff"
+# export COLOR_WHITE="0xffc0caf5" #"#c0caf5"
 
-export COLOR_BLACK="0xff414868" #"#414868"
-export COLOR_RED="0xfff7768e" #f7768e"
-export COLOR_GREEN="0xff73daca" #"#73daca"
-export COLOR_YELLOW="0xffe0af68" #"#e0af68"
-export COLOR_BLUE="0xff7aa2f7" #"#7aa2f7"
-export COLOR_PURPLE="0xffbb9af7" #"#bb9af7"
-export COLOR_CYAN="0xff7dcfff" #"#7dcfff"
-export COLOR_WHITE="0xffc0caf5" #"#c0caf5"
+# Nordfox
+export COLOR_FOREGROUND="0xffc7cdd9" # "#c7cdd9"
+export COLOR_BACKGROUND="0xb0232831" # "#232831" but 69% transparent
 
+export COLOR_BLACK="0xff3b4252" # "#3b4252"
+export COLOR_RED="0xffbf616a" # "#bf616a"
+export COLOR_GREEN="0xffa3be8c" #"#a3be8c"
+export COLOR_YELLOW="0xffebcb8b" #"#ebcb8b"
+export COLOR_BLUE="0xff81a1c1" #"#81a1c1"
+export COLOR_PURPLE="0xffbf88bc" #"#bf88bc"  this is actually pink
+export COLOR_CYAN="0xff7dcfff" #"#88c0d0"
+export COLOR_WHITE="0xffe5e9f0" #"#e5e9f0"
