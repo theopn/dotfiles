47e70386bbef81a81fc00df6e9f22434de9e981e
Theo P <theo.park.n@gmail.com>
2025-05-19 15:15:01 +0900

    feat(nvim): add oil.nvim floating shortcut

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,9 +3,41 @@ return {
   dependencies = {
     { "nvim-tree/nvim-web-devicons" },
   },
-  opts = {
-    view_options = {
-      show_hidden = true,
-    }
-  },
+  config = function()
+    local oil = require("oil")
+
+    -- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#show-cwd-in-the-winbar
+    -- Declare a global function to retrieve the current directory
+    function _G.get_oil_winbar()
+      local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+      local dir = oil.get_current_dir(bufnr)
+      if dir then
+        dir = vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        dir = vim.api.nvim_buf_get_name(0)
+      end
+
+      return table.concat({
+        "%#Normal#",
+
+        "%#MiniStatuslineModeOther#",
+        "[OIL.NVIM] ",
+
+        dir,
+      })
+    end
+
+    oil.setup({
+      view_options = {
+        show_hidden = true,
+      },
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+    })
+
+    vim.keymap.set("n", "<leader>b", oil.toggle_float,
+      { desc = "Toggle File [B]rowser (Oil.nvim)" })
+  end,
 }
