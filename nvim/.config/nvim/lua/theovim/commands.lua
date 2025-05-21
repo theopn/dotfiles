94d507aad7e2ad44da790174e885387170a1ea0c
Theo P <theo.park.n@gmail.com>
2025-05-21 14:34:53 +0900

    refactor(nvim): create a shortcut for creating autocmd, augroup, and user cmd

--- nvim/.config/nvim/lua/theovim/commands.lua
+++ nvim/.config/nvim/lua/theovim/commands.lua
@@ -1,5 +1,7 @@
 -- Theovim user commands
 
+local cmd = vim.api.nvim_create_user_command
+
 --- Trims trailing whitespace
 --- \s: white space char, \+ :one or more, $: end of the line, e: suppresses warning when no match found, c: confirm
 local function trim_whitespace()
@@ -7,10 +9,10 @@ local function trim_whitespace()
   vim.cmd([[keeppatterns %s/\s\+$//ec]])
   vim.fn.winrestview(win_save)
 end
-vim.api.nvim_create_user_command("TrimWhitespace", trim_whitespace, { nargs = 0 })
+cmd("TrimWhitespace", trim_whitespace, { nargs = 0 })
 
 -- Changes current working directory
-vim.api.nvim_create_user_command("CD", ":lcd %:h", { nargs = 0 })
+cmd("CD", ":lcd %:h", { nargs = 0 })
 
 -- Sets the buffer to readonly and not modifiable state
-vim.api.nvim_create_user_command("RO", ":setlocal readonly nomodifiable", { nargs = 0 })
+cmd("RO", ":setlocal readonly nomodifiable", { nargs = 0 })
