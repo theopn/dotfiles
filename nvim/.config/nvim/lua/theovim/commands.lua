d0ad901950a13862ccdeebde2d790e2ad56981f4
Theo P <theo.park.n@gmail.com>
2025-05-21 12:10:32 +0900

    feat(nvim): add readonly + nomodifiable command

--- nvim/.config/nvim/lua/theovim/commands.lua
+++ nvim/.config/nvim/lua/theovim/commands.lua
@@ -11,3 +11,6 @@ vim.api.nvim_create_user_command("TrimWhitespace", trim_whitespace, { nargs = 0 
 
 -- Changes current working directory
 vim.api.nvim_create_user_command("CD", ":lcd %:h", { nargs = 0 })
+
+-- Sets the buffer to readonly and not modifiable state
+vim.api.nvim_create_user_command("RO", ":setlocal readonly nomodifiable", { nargs = 0 })
