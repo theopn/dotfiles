0c89780acf53179c0da68e2e11d6c49889579640
Theo P <theo.park.n@gmail.com>
2025-05-21 15:46:19 +0900

    rafactor(nvim): change `vim.opt` to `vim.o` where possible         https://github.com/neovim/neovim/issues/20107

--- nvim/.config/nvim/lua/theovim/opt.lua
+++ nvim/.config/nvim/lua/theovim/opt.lua
@@ -1,66 +1,67 @@
 -- Theovim options
 -- Excludes default Neovim options:
 -- https://neovim.io/doc/user/vim_diff.html#nvim-defaults
-local opt          = vim.opt
+local o           = vim.o
 
 -- Tab
-opt.softtabstop    = 0    --> How many chracters the /cursor moves/ with <TAB> and <BS> -- 0 to disable
-opt.expandtab      = true --> Use space instead of tab
-opt.shiftwidth     = 2    --> Number of spaces to use for auto-indentation, <<, >>, etc.
-opt.shiftround     = true --> Make the indentation to a multiple of shiftwidth when using < or >
+o.softtabstop     = 0    --> How many chracters the /cursor moves/ with <TAB> and <BS> -- 0 to disable
+o.expandtab       = true --> Use space instead of tab
+o.shiftwidth      = 2    --> Number of spaces to use for auto-indentation, <<, >>, etc.
+o.shiftround      = true --> Make the indentation to a multiple of shiftwidth when using < or >
 
 -- Location in the buffer
-opt.number         = true
-opt.relativenumber = true
-opt.cursorline     = true
-opt.cursorlineopt  = "number" --> line, screenline, both (i.e., "number,line")
-opt.cursorcolumn   = true
+o.number          = true
+o.relativenumber  = true
+o.cursorline      = true
+o.cursorlineopt   = "number" --> line, screenline, both (i.e., "number,line")
+o.cursorcolumn    = true
 
 -- Search and replace
-opt.ignorecase     = true    --> Ignore case in search
-opt.smartcase      = true    --> /smartcase -> apply ignorecase | /sMartcase -> do not apply ignorecase
-opt.inccommand     = "split" --> show the substitution in a split window
+o.ignorecase      = true    --> Ignore case in search
+o.smartcase       = true    --> /smartcase -> apply ignorecase | /sMartcase -> do not apply ignorecase
+o.inccommand      = "split" --> show the substitution in a split window
 
 -- Split
-opt.splitright     = true --> Vertical split created right
-opt.splitbelow     = true --> Horizontal split created below
+o.splitright      = true --> Vertical split created right
+o.splitbelow      = true --> Horizontal split created below
 
 -- UI
-opt.signcolumn     = "yes" --> Render signcolumn always to prevent text shifting
-opt.scrolloff      = 7     --> Keep minimum x number of screen lines above and below the cursor
-opt.termguicolors  = true  --> Enables 24-bit RGB color in the TUI
-opt.showtabline    = 2     --> 0: never, 1: >= 2 tabs, 2: always
-opt.laststatus     = 3     --> 0: never, 1: >= 2 windows, 2: always, 3: always and have one global statusline
+o.signcolumn      = "yes" --> Render signcolumn always to prevent text shifting
+o.scrolloff       = 7     --> Keep minimum x number of screen lines above and below the cursor
+o.termguicolors   = true  --> Enables 24-bit RGB color in the TUI
+o.showtabline     = 2     --> 0: never, 1: >= 2 tabs, 2: always
+o.laststatus      = 3     --> 0: never, 1: >= 2 windows, 2: always, 3: always and have one global statusline
 
 -- Char rendering
-opt.list           = true --> Render special char in listchars
-opt.listchars      = { tab = "⇥ ", trail = "␣", nbsp = "⍽", leadmultispace = "┊ ", }
-opt.showbreak      = "↪" --> Render beginning of wrapped lines
-opt.breakindent    = true --> Wrapped line will have the same indentation level as the beginning of the line
+o.list            = true --> Render special char in listchars
+vim.opt.listchars = { tab = "⇥ ", trail = "␣", nbsp = "⍽", leadmultispace = "┊ ", }
+o.showbreak       = "↪" --> Render beginning of wrapped lines
+o.breakindent     = true --> Wrapped line will have the same indentation level as the beginning of the line
 
 -- Spell
-opt.spell          = false    --> autocmd will enable spellcheck in Tex or markdown
-opt.spelllang      = { "en" }
-opt.spellsuggest   = "best,8" --> Suggest 8 words for spell suggestion
-opt.spelloptions   = "camel"  --> Consider CamelCase when checking spelling
+o.spell           = false    --> autocmd will enable spellcheck in Tex or markdown
+vim.opt.spelllang = { "en", }
+o.spellsuggest    = "best,8" --> Suggest 8 words for spell suggestion
+o.spelloptions    = "camel"  --> Consider CamelCase when checking spelling
 
 -- Fold
-opt.foldenable     = false                        --> Open all folds until I close them using zc/zC or update using zx
-opt.foldmethod     = "expr"                       --> Use `foldexpr` function for folding
-opt.foldexpr       = "nvim_treesitter#foldexpr()" --> Treesitter folding
---foldlevel      = 2                            --> Ignore n - 1 level fold
+o.foldenable      = false                        --> Open all folds until I close them using zc/zC or update using zx
+o.foldmethod      = "expr"                       --> Use `foldexpr` function for folding
+o.foldexpr        = "nvim_treesitter#foldexpr()" --> Treesitter folding
+--o.foldlevel      = 2                            --> Ignore n - 1 level fold
 
 -- Update time
-opt.updatetime     = 250
-opt.timeoutlen     = 300
+o.updatetime      = 250
+o.timeoutlen      = 300
 
 -- Window size
-opt.winminwidth    = 3
+o.winminwidth     = 3
 
 -- Completion
-opt.wildmode       = "noselect:full"            --> behavior of <TAB> in cmd completion. Do not pre-select, match full completion
-opt.completeopt    = "noselect,menu,menuone,popup" --> no pre-insert or pre-select, show menu even w/ one item, use popup
+o.wildmode        =
+"noselect:full"                                   --> behavior of <TAB> in cmd completion. Do not pre-select, match full completion
+o.completeopt     = "noselect,menu,menuone,popup" --> no pre-insert or pre-select, show menu even w/ one item, use popup
 
 -- Others
-opt.mouse          = "a"
-opt.confirm        = true --> Confirm before exiting with unsaved bufffer(s)
+o.mouse           = "a"
+o.confirm         = true --> Confirm before exiting with unsaved bufffer(s)
