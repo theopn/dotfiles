830c1bfa7ce93809f4fee12dbb8c4cd68d2fbdfa
Theo P <theo.park.n@gmail.com>
2025-05-22 12:39:57 +0900

    fix(nvim): increase the number of fields being displayed in the hover of Lua LS

--- nvim/.config/nvim/lsp/luals.lua
+++ nvim/.config/nvim/lsp/luals.lua
@@ -1,12 +1,28 @@
+-- Default 50: https://luals.github.io/wiki/settings/#hover
+local maxNumFieds = 100
+
 ---@type vim.lsp.Config
 return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
-  --on_init = require("util").lua_ls_on_init,
+  root_markers = { ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+    ".git"
+  },
   settings = {
     Lua = {
-      telemetry = { enable = false },
+      hover = {
+        enumsLimit = maxNumFieds,
+        previewFields = maxNumFieds,
+      },
+      telemetry = {
+        enable = false
+      },
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true)
       }
