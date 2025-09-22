65c82b5aeded759b491e488e440e84e6162c59bf
Theo P <theo.park.n@gmail.com>
2025-09-22 16:03:10 -0400

    feat(nvim): add --shell-escape flag for Vimtex

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -2,8 +2,21 @@ return {
   "lervag/vimtex",
   config = function()
     vim.g.tex_flavor = "latex"
-    vim.g.vimtex_view_method = "skim"   --> macOS
-    --vim.g.vimtex_view_method = "zathura" --> Linux
+
+    -- vim.g.vimtex_view_method = "skim"   --> macOS
+    vim.g.vimtex_view_method = "zathura" --> Linux
+
+    -- Add --shell-escape flag when external tools are required
+    -- e.g., minted package requiring `pygmentize`
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        "-shell-escape",
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode"
+      },
+    }
   end,
   ft = { "plaintex", "tex" },
 }
