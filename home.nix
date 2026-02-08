e93996339916a1d7401bee27dc25e3eb6a4e0618
Theo P <theo.park.n@gmail.com>
2026-02-07 20:45:25 -0500

    feat(nix): add some CLI dependencies

--- home.nix
+++ home.nix
@@ -6,19 +6,46 @@
       inherit username;
       homeDirectory = "/home/" + username;
 
-      # packages = with pkgs; [
-      # ];
+      packages = with pkgs; [
+        bat
+        chafa
+        exiftool
+        fastfetch
+        fd
+        ffmpeg
+        figlet
+        fzf
+        git-filter-repo
+        hugo
+        imagemagick
+        lua
+        neovim
+        nodejs_24
+        R
+        ripgrep
+        cargo
+        rustc
+        tree
+        tmux
+        vim
+        wget
+
+        neovide
+
+
+        texlive.combined.scheme-full
+      ];
 
       file = {
         ".config/lf".source = ./lf/.config/lf;
+        ".config/fastfetch".source = ./fastfetch/.config/fastfetch;
+        ".tmux.conf".source = ./tmux/.tmux.conf;
       };
 
       stateVersion = "25.11";
     };
 
     programs.home-manager.enable = true;
-
-    programs.bat.enable = true;
 
     programs.btop = {
       enable = true;
