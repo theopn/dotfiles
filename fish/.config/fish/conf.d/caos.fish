547f7c741c538b8f502366c409e8e84ea46b96e0
Theo P <theo.park.n@gmail.com>
2026-02-23 16:35:13 -0500

    refactor(fish): separate abbreviation files into multiple parts, including CAOS (productivity system) and Bash keybind emulation

---
+++ fish/.config/fish/conf.d/caos.fish
@@ -0,0 +1,14 @@
+# CAOS is a cringe name I gave to my productivity system
+# (Capture Act Organize Sustain).
+# I mainly use nvim-org-mode for task management ("Capture", "Act", and "Organize" parts),
+# and various shell scripts + Vimwiki for journaling and habit tracking ("Sustain" part)
+
+
+# personal journal entry for the day
+# - If I just pass the subshell without escaping (i.e., `+/$(date)`),
+#   then Fish will evaluate in the shell startup and the fixed date.
+# - I do not want to overcomplicate the command so I keep the first one as-is
+#   (since year changing does not happen very often),
+#   but I do not want to reopen the shell every 24 hours
+abbr -a dw vim \"$CACHE_DIR/dw-$(date +%Y)/index.md\" +/\$\(date +%Y-%m-%d\)
+
