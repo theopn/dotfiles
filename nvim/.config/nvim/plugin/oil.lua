c4c515c3610f9bf51424e8951b420cfdb8529dee
Theo Park <theo.park.n@gmail.com>
2026-05-16 23:29:59 -0400

    fix(nvim): my custom winbar being applied to Oil windows when it shouldn't. It used to be handled by a custom global function that displays CWD, but I realized that because CWD is already in the floating window decoration, I can just... set it to nil

--- nvim/.config/nvim/plugin/oil.lua
+++ nvim/.config/nvim/plugin/oil.lua
@@ -4,35 +4,13 @@ vim.pack.add({
 
 local oil = require("oil")
 
--- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#show-cwd-in-the-winbar
--- Declare a global function to retrieve the current directory
-function _G.get_oil_winbar()
-  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-  local dir = oil.get_current_dir(bufnr)
-  if dir then
-    dir = vim.fn.fnamemodify(dir, ":~")
-  else
-    -- If there is no current directory (e.g. over ssh), just show the buffer name
-    dir = vim.api.nvim_buf_get_name(0)
-  end
-
-  return table.concat({
-    "%#Normal#",
-
-    "%#MiniStatuslineModeOther#",
-    "[OIL.NVIM] ",
-
-    dir,
-  })
-end
-
 oil.setup({
   delete_to_trash = true,
   view_options = {
     show_hidden = true,
   },
   win_options = {
-    winbar = "%!v:lua.get_oil_winbar()",
+    winbar = nil,
   },
 })
 
